be4d9add4501ba00499fc6a7bced4361e5cc7f4c
Nipun Birla <nipun.birla@gmail.com>
2017-06-18 21:27:09 +0530

    resource values added

--- boxloader2/src/main/java/com/nipunbirla/boxloader/BoxLoaderView.java
+++ boxloader2/src/main/java/com/nipunbirla/boxloader/BoxLoaderView.java
@@ -1,10 +1,10 @@
 package com.nipunbirla.boxloader;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.util.AttributeSet;
@@ -18,11 +18,15 @@ import com.nipunbirla.boxloaderlib.R;
 
 public class BoxLoaderView extends View {
 
-    private static final int FRAME_RATE = 5;
-    private static final int DEFAULT_SPEED = 5;
-    private static final int STROKE_WIDTH = 20;
-    private static final int INNER_MARGIN = 20;
+    private static final int FRAME_RATE = 2;
+    private static final int DEFAULT_SPEED = 10;
+    private static final int DEFAULT_STROKE_WIDTH = 20;
+    private static final int DEFAULT_STROKE_COLOR = Color.WHITE;
+    private static final int DEFAULT_LOADER_COLOR = Color.BLUE;
 
+    private int speed;
+    private int strokeWidth;
+    private int strokeColor, loaderColor;
     private boolean dirChange = false;
     private Paint paint;
     private Box box, outBox;
@@ -31,40 +35,82 @@ public class BoxLoaderView extends View {
 
     public BoxLoaderView(Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
 
     public BoxLoaderView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
 
-    public BoxLoaderView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
-    private void init(Context context){
+    private void init(Context context, AttributeSet attrs){
         handler = new Handler();
         paint = new Paint();
         paint.setColor(Color.BLUE);
+
+        if(attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs,
+                    R.styleable.Options, 0, 0);
+            strokeColor = a.getColor(R.styleable.Options_strokeColor, DEFAULT_STROKE_COLOR);
+            loaderColor = a.getColor(R.styleable.Options_loaderColor, DEFAULT_LOADER_COLOR);
+            strokeWidth = a.getInt(R.styleable.Options_strokeWidth, DEFAULT_STROKE_WIDTH);
+            speed = a.getInt(R.styleable.Options_speed, DEFAULT_SPEED);
+            a.recycle();
+        } else {
+            strokeColor = DEFAULT_STROKE_COLOR;
+            loaderColor = DEFAULT_LOADER_COLOR;
+            strokeWidth = DEFAULT_STROKE_WIDTH;
+            speed = DEFAULT_SPEED;
+        }
+    }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    public void setStrokeWidth(int strokeWidth){
+        this.strokeWidth = strokeWidth;
+    }
+
+    public void setStrokeColor(int color){
+        strokeColor = color;
+    }
+
+    public void setLoaderColor(int color){
+        loaderColor = color;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         if(outBox == null){
-            outBox = new Box(0,0,canvas.getWidth(), canvas.getHeight(), Color.WHITE, 10);
-            outBox.getPaint().setStrokeWidth(STROKE_WIDTH);
+            outBox = new Box(0,0,canvas.getWidth(), canvas.getHeight(), strokeColor, 10);
+            outBox.getPaint().setStrokeWidth(strokeWidth);
         }
         canvas.drawRect(outBox.getLeft(), outBox.getTop(), outBox.getRight(), outBox.getBottom(), outBox.getPaint());
         if(box == null){
-            box = new Box(STROKE_WIDTH,STROKE_WIDTH, canvas.getWidth()/2 - STROKE_WIDTH, canvas.getHeight()/2 - STROKE_WIDTH, Color.BLUE, 10);
-            box.setDx(DEFAULT_SPEED);
-            box.setDy(DEFAULT_SPEED);
+            box = new Box(strokeWidth, strokeWidth, canvas.getWidth()/2 - strokeWidth, canvas.getHeight()/2 - strokeWidth, loaderColor, 10);
+            box.setDx(speed);
+            box.setDy(speed);
         }
-        dirChange = box.bounce(canvas);
+        dirChange = box.bounce(canvas, strokeWidth);
+        rectifyBoundaries(canvas, box);
         canvas.drawRect(box.getLeft(), box.getTop(), box.getRight(), box.getBottom(), box.getPaint());
         handler.postDelayed(r, dirChange ? FRAME_RATE * 20 : FRAME_RATE);
+    }
+
+    private void rectifyBoundaries(Canvas canvas, Box box) {
+        if(box.getLeft() < strokeWidth){
+            box.getrect().left = strokeWidth;
+        }
+        if(box.getTop() < strokeWidth){
+            box.getrect().top = strokeWidth;
+        }
+        if(box.getRight() > canvas.getWidth() - strokeWidth){
+            box.getrect().right = canvas.getWidth() - strokeWidth;
+        }
+        if(box.getBottom() > canvas.getHeight() - strokeWidth){
+            box.getrect().bottom = canvas.getHeight() - strokeWidth;
+        }
     }
 
     private Runnable r = new Runnable() {
@@ -167,48 +213,52 @@ public class BoxLoaderView extends View {
             rect.bottom -= dy;
         }
 
+        public Rect getrect(){
+            return rect;
+        }
+
         //Bounce of edge
-        public boolean bounce(Canvas canvas){
+        public boolean bounce(Canvas canvas, int strokeWidth){
             switch (dir){
                 case 0:
-                    if(rect.right <= canvas.getWidth() - STROKE_WIDTH){
+                    if(rect.right < canvas.getWidth() - strokeWidth){
                         increaseRight();
                     } else {
                         increaseLeft();
-                        if(rect.left >= canvas.getWidth()/2 + STROKE_WIDTH){
+                        if(rect.left > canvas.getWidth()/2){
                             dir++;
                             return true;
                         }
                     }
                     break;
                 case 1:
-                    if(rect.bottom <= canvas.getHeight() - STROKE_WIDTH){
+                    if(rect.bottom < canvas.getHeight() - strokeWidth){
                         increaseBottom();
                     } else {
                         increaseTop();
-                        if(rect.top >= canvas.getHeight()/2 + STROKE_WIDTH){
+                        if(rect.top > canvas.getHeight()/2){
                             dir++;
                             return true;
                         }
                     }
                     break;
                 case 2:
-                    if(rect.left >= STROKE_WIDTH){
+                    if(rect.left > strokeWidth){
                         decreaseLeft();
                     } else {
                         decreaseRight();
-                        if(rect.right <= canvas.getWidth()/2 - STROKE_WIDTH){
+                        if(rect.right < canvas.getWidth()/2){
                             dir++;
                             return true;
                         }
                     }
                     break;
                 case 3:
-                    if(rect.top >= STROKE_WIDTH){
+                    if(rect.top > strokeWidth){
                         decreaseTop();
                     } else {
                         decreaseBottom();
-                        if(rect.bottom <= canvas.getHeight()/2 - STROKE_WIDTH){
+                        if(rect.bottom < canvas.getHeight()/2){
                             dir = 0;
                             return true;
                         }
